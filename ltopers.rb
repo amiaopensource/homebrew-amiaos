a5b82e9a7a0d2d1f263a7314b4c7e10f7c805ae0
Dave Rice <dericed@yahoo.com>
2014-05-06 22:12:00 -0400

    ltopers.rb: use specific version

--- ltopers.rb
+++ ltopers.rb
@@ -2,6 +2,9 @@ require 'formula'
 
 class Ltopers < Formula
   homepage 'https://github.com/amiaopensource/ltopers'
+  url 'http://github.com/amiaopensource/ltopers/archive/v0.0.1.tar.gz'
+  md5 'b43dffc882249c42232ad0856fb1e0c1'
+  
   head 'git://github.com/amiaopensource/ltopers.git'
 
   depends_on 'xmlstarlet'
