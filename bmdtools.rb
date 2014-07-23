a2f15a6d8f073d379024307fa603e1fc66ca5916
bastibeckr <post@bastibeckr.com>
2014-07-23 21:41:36 +0200

    add SYS=Darwin
    Automatic detection does not work for me.

--- bmdtools.rb
+++ bmdtools.rb
@@ -11,7 +11,7 @@ class Bmdtools < Formula
   depends_on 'libav' => :build
  
   def install
-     system "make", "SDK_PATH=/usr/local/include"
+     system "make", "SDK_PATH=/usr/local/include", "SYS=Darwin"
      bin.install 'bmdcapture'
      bin.install 'bmdgenlock'
      bin.install 'bmdplay'
