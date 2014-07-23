3fbfb5baf99e9b4151a9d79bdbe3517ee6b5d95a
bastibeckr <post@bastibeckr.com>
2014-07-23 21:40:53 +0200

    Switch to standard environment
    Homebrew's superenv seems to break things which results in missing libav dependency.

--- bmdtools.rb
+++ bmdtools.rb
@@ -1,13 +1,15 @@
 require 'formula'
-
+ 
 class Bmdtools < Formula
   homepage 'http://github.com/lu-zero/bmdtools'
   head 'https://github.com/lu-zero/bmdtools.git'
-
-  depends_on 'pkg-config'
-  depends_on 'amiaopensource/amiaos/decklinksdk'
-  depends_on 'libav'
-
+ 
+  env :std
+ 
+  depends_on 'pkg-config' => :build
+  depends_on 'amiaopensource/amiaos/decklinksdk' => :build
+  depends_on 'libav' => :build
+ 
   def install
      system "make", "SDK_PATH=/usr/local/include"
      bin.install 'bmdcapture'
