316a99b12486c34ed25922eeb8b08f60748f173f
Dave Rice <dave@dericed.com>
2014-08-13 12:42:35 -0400

    vrecord: version 0.1

--- vrecord.rb
+++ vrecord.rb
@@ -2,12 +2,16 @@ require 'formula'
 
 class Vrecord < Formula
   homepage 'https://github.com/amiaopensource/vrecord'
+  url "https://github.com/amiaopensource/vrecord/archive/vrecord_v0.1.zip"
+  sha256 "9b6744c406dcf3b7c1f188ec38c17fa728e9f69853607002f21689bcf75c1ee0"
   head 'https://github.com/amiaopensource/vrecord.git'
 
   depends_on 'amiaopensource/amiaos/decklinksdk'
+  depends_on "xmlstarlet" => :recommended
 
   def install
      bin.install 'vrecord'
      bin.install 'vstop'
+     bin.install 'shotcutsaveas'
   end
 end
