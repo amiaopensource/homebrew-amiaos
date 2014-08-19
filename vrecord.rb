065fd0846d04bffd5247a03b9297b6dd1ee813b4
Dave Rice <dave@dericed.com>
2014-08-19 15:47:11 -0400

    vrecord v0.2

--- vrecord.rb
+++ vrecord.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class Vrecord < Formula
   homepage 'https://github.com/amiaopensource/vrecord'
-  url "https://github.com/amiaopensource/vrecord/archive/vrecord_v0.1.zip"
-  sha256 "9b6744c406dcf3b7c1f188ec38c17fa728e9f69853607002f21689bcf75c1ee0"
+  url "https://github.com/amiaopensource/vrecord/archive/vrecord_v0.2.zip"
+  sha256 "74ec29a65ed7e9a77fbc79f45e4f44fa12173c723e4fbc8efe05d14cb139c616"
   head 'https://github.com/amiaopensource/vrecord.git'
 
   depends_on 'amiaopensource/amiaos/decklinksdk'
   depends_on "xmlstarlet" => :recommended
+  depends_on "ffmpeg"
 
   def install
      bin.install 'vrecord'
