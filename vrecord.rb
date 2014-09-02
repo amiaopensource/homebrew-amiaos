c903f8a0c7328be130e9f832906e75c4fa103f80
Dave Rice <dave@dericed.com>
2014-09-02 15:38:15 -0400

    fix dl url for vrecord v0.3

--- vrecord.rb
+++ vrecord.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Vrecord < Formula
   homepage 'https://github.com/amiaopensource/vrecord'
-  url "https://github.com/amiaopensource/vrecord/archive/vrecord_v0.3.zip"
+  url "https://github.com/amiaopensource/vrecord/archive/v0.3.zip"
   sha256 "6838189374ec8912f271094989ab7a6bee791e1da86366389fce54321e08c10a"
   head 'https://github.com/amiaopensource/vrecord.git'
 
