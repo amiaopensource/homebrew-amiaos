98b4c157efdbb0ca42bb2515b80d9dcb71cdf17b
Dave Rice <dave@dericed.com>
2014-09-09 14:37:46 -0400

    vrecord v0.5

--- vrecord.rb
+++ vrecord.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vrecord < Formula
   homepage 'https://github.com/amiaopensource/vrecord'
-  url "https://github.com/amiaopensource/vrecord/archive/v0.4.zip"
-  sha256 "a05246e946f4292c579baddb12691fdeb45ff32b39d22f8c5964f1e9e22c4a44"
+  url "https://github.com/amiaopensource/vrecord/archive/v0.5.zip"
+  sha256 "02afb1949b52a90d41d481efcf534718037328ab4c69727515b574069065e8d6"
   head 'https://github.com/amiaopensource/vrecord.git'
 
   depends_on 'amiaopensource/amiaos/bmdtools'
