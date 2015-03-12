e52c1f84f89a4e0a499f2f49d67e83c36c23e33f
Dave Rice <dave@dericed.com>
2015-03-12 17:06:27 -0400

    v0.0.2

--- ltopers.rb
+++ ltopers.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ltopers < Formula
   homepage 'https://github.com/amiaopensource/ltopers'
-  url 'http://github.com/amiaopensource/ltopers/archive/v0.0.1.tar.gz'
-  sha256 '947463bcd99a07f9bab26b8c35c037f4ecc5a0d565aecd0d00c870f59e59b949'
+  url 'http://github.com/amiaopensource/ltopers/archive/v0.0.2.tar.gz'
+  sha256 '62f812d308121fe4bd6cfdbcbefaa37b1068c6cee907ced4ed01f26b24c43394'
   
   head 'git://github.com/amiaopensource/ltopers.git'
 
