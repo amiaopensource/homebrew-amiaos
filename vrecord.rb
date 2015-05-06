5b93c14bdc2324b74d1404ae3cedd65d0baf533d
Dave Rice <dave@dericed.com>
2015-05-06 09:59:43 -0400

    fixing v0.7 release

--- vrecord.rb
+++ vrecord.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Vrecord < Formula
   homepage 'https://github.com/amiaopensource/vrecord'
-  url "https://github.com/amiaopensource/vrecord/archive/v0.6.1.zip"
+  url "https://github.com/amiaopensource/vrecord/archive/v0.7.zip"
   sha256 "bdae8dd7c6dd129543b28d998b4b1692711feb743440d78d2d0284d33cabd768"
   head 'https://github.com/amiaopensource/vrecord.git'
 
