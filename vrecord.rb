f9e6584adecdbfb7d76258edc47b89856904f095
Dave Rice <dave@dericed.com>
2015-07-28 16:58:42 -0400

    v.7.1 and shameful hack to install cask dependency

--- vrecord.rb
+++ vrecord.rb
@@ -2,16 +2,18 @@ require 'formula'
 
 class Vrecord < Formula
   homepage 'https://github.com/amiaopensource/vrecord'
-  url "https://github.com/amiaopensource/vrecord/archive/v0.7.zip"
-  sha256 "833bcb45f39c40302b361c9c8f50e9751cf0294884b73e2e6e902d1984a10c96"
+  url "https://github.com/amiaopensource/vrecord/archive/v0.7.1.zip"
+  sha256 "d68b6c41534f589b3d9f58be01ce48f17fd81518c286ae3129a349f6d94c261f"
   head 'https://github.com/amiaopensource/vrecord.git'
 
   depends_on 'amiaopensource/amiaos/bmdtools'
   depends_on "xmlstarlet" => :recommended
   depends_on "sdl"
   depends_on "ffmpeg" => [ 'with-ffplay', 'with-openjpeg', 'with-libass', 'with-freetype', 'with-faac' ]
+  depends_on "caskroom/cask/brew-cask"
 
   def install
+     system "brew", "cask", "install", "pashua"
      bin.install 'vrecord'
      bin.install 'vstop'
      bin.install 'shotcutsaveas'
