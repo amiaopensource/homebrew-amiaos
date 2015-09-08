d85c5f1012b8f17a55bec049cd823cae2e8693ed
Dave Rice <dave@dericed.com>
2015-09-08 11:39:45 -0400

    deckcontrol, fix sha256 for version 0.3

--- deckcontrol.rb
+++ deckcontrol.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Deckcontrol < Formula
     homepage 'https://github.com/bavc/deckcontrol'
     url "https://github.com/bavc/deckcontrol/archive/v0.3.zip"
-    sha256 "2c9bfda0d08a85e2057eeccc24f371183b3f524951ab399773cab20de89a0d77"
+    sha256 "6d84028726abc4f68c0b122a48cfea261e6bbe8e642ca66a71285ccb500c4933"
     head 'https://github.com/bavc/deckcontrol.git'
 
     depends_on 'amiaopensource/amiaos/decklinksdk' => :build
