743250cdee4d6a2c8b1478cf440b736e77f871d7
Jonathan Farbowitz <jfarbowitz@gmail.com>
2015-11-13 09:53:47 -0500

    updated SHA256

--- vrecord.rb
+++ vrecord.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Vrecord < Formula
   homepage 'https://github.com/amiaopensource/vrecord'
   url "https://github.com/amiaopensource/vrecord/archive/v0.7.9.zip"
-  sha256 "72f6a99b70d7b9fba713ee169269c2e909a4cfa8d5f0d93b6534f355fceac0ca"
+  sha256 "0b4eaddd704729a79b9120a908c8317654c82b57ebfb5b3d0ea04f06cf1d1494"
   head 'https://github.com/amiaopensource/vrecord.git'
 
   depends_on "sdl"
