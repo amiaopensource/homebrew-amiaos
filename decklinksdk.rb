36c218d0af33f7c8bee46ff18e884ab4d1738d52
Dave Rice <dave@dericed.com>
2016-02-24 14:13:08 -0500

    add mirror of the decklink sdk
    the main url is down :(
    waiting to hear from blackmagic

--- decklinksdk.rb
+++ decklinksdk.rb
@@ -3,7 +3,9 @@ require 'formula'
 class Decklinksdk < Formula
   homepage 'http://www.blackmagicdesign.com/support/sdks/'
   url 'http://software.blackmagicdesign.com/SDK/Blackmagic_DeckLink_SDK_10.1.4.zip'
+  mirror 'http://slackware.uk/sbosrcarch/by-name/multimedia/vlc/Blackmagic_DeckLink_SDK_10.1.4.zip'
   sha256 '65d4517a454f3809fffea1c2e1ceab11887e1e4530f278078a63cd52bb24ff73'
+  rev 1
 
   def install
       
