a0c25dcb55650fefdaeaaab4d3311caf8cd587cc
Dave Rice <dave@dericed.com>
2016-02-25 12:07:16 -0500

    promote mirror to url, since main url is still down

--- decklinksdk.rb
+++ decklinksdk.rb
@@ -1,10 +1,9 @@
 class Decklinksdk < Formula
   desc "The Blackmagic Design Decklink Software Development Kit libraries"
   homepage "https://www.blackmagicdesign.com/support/family/capture-and-playback"
-  url "http://software.blackmagicdesign.com/SDK/Blackmagic_DeckLink_SDK_10.1.4.zip"
-  mirror "http://slackware.uk/sbosrcarch/by-name/multimedia/vlc/Blackmagic_DeckLink_SDK_10.1.4.zip"
+  url "http://slackware.uk/sbosrcarch/by-name/multimedia/vlc/Blackmagic_DeckLink_SDK_10.1.4.zip"
   sha256 "65d4517a454f3809fffea1c2e1ceab11887e1e4530f278078a63cd52bb24ff73"
-  revision 2
+  revision 3
 
   def install
     include.install Dir["Mac/include/*"]
