1200a399b0bbfa7ade09923dc41490fb8945bd03
dericed <dave@dericed.com>
2016-02-24 23:44:14 -0500

    decklinksdk installer updates
    advice from https://github.com/Homebrew/homebrew/pull/41435

--- decklinksdk.rb
+++ decklinksdk.rb
@@ -1,22 +1,12 @@
-require 'formula'
-
 class Decklinksdk < Formula
-  homepage 'http://www.blackmagicdesign.com/support/sdks/'
-  url 'http://software.blackmagicdesign.com/SDK/Blackmagic_DeckLink_SDK_10.1.4.zip'
-  mirror 'http://slackware.uk/sbosrcarch/by-name/multimedia/vlc/Blackmagic_DeckLink_SDK_10.1.4.zip'
-  sha256 '65d4517a454f3809fffea1c2e1ceab11887e1e4530f278078a63cd52bb24ff73'
-  revision 1
+  desc "The Blackmagic Design Decklink Software Development Kit libraries"
+  homepage "https://www.blackmagicdesign.com/support/family/capture-and-playback"
+  url "http://software.blackmagicdesign.com/SDK/Blackmagic_DeckLink_SDK_10.1.4.zip"
+  mirror "http://slackware.uk/sbosrcarch/by-name/multimedia/vlc/Blackmagic_DeckLink_SDK_10.1.4.zip"
+  sha256 "65d4517a454f3809fffea1c2e1ceab11887e1e4530f278078a63cd52bb24ff73"
+  revision 2
 
   def install
-      
-    # blackmagic's DeckControl bin names conflict with deckcontrol by BAVC
-    mv "Mac/Samples/bin/DeckControl", "Mac/Samples/bin/DeckControlsdk"
-    
-    bin.install 'Mac/Samples/bin/BypassControl'
-    bin.install 'Mac/Samples/bin/CaptureFromTape'
-    bin.install 'Mac/Samples/bin/DeckControlsdk'
-    bin.install 'Mac/Samples/bin/DeviceList'
-    bin.install 'Mac/Samples/bin/ExportToTape'
-    include.install Dir['Mac/include/*']
+    include.install Dir["Mac/include/*"]
   end
 end
