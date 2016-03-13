bd487a7132ca14e6ae40a76c52a5884cb69e3ea9
Dave Rice <dave@dericed.com>
2016-03-13 17:17:52 -0400

    libass isn't needed

--- vrecord.rb
+++ vrecord.rb
@@ -5,10 +5,11 @@ class Vrecord < Formula
   url "https://github.com/amiaopensource/vrecord/archive/v0.7.11.zip"
   sha256 "92fd5181687eff6358c60fe98d33400dd6cd6bc33a49ec5d0645a04fc3dbfd61" 
   head 'https://github.com/amiaopensource/vrecord.git'
+  revision 1
 
   depends_on "sdl"
   depends_on "cowsay"
-  depends_on "ffmpeg" => [ 'with-ffplay', 'with-libass', 'with-freetype' ]
+  depends_on "ffmpeg" => [ 'with-ffplay', 'with-freetype' ]
   depends_on 'amiaopensource/amiaos/bmdtools'
   depends_on "xmlstarlet" => :recommended
   depends_on "caskroom/cask/brew-cask"
