fdc5aa518f458ffcdbb64afd11e664d4ce3af743
Dinah <dinahrhandel@gmail.com>
2016-03-29 11:10:26 -0400

    Update ltopers.rb

--- ltopers.rb
+++ ltopers.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ltopers < Formula
   homepage 'https://github.com/amiaopensource/ltopers'
-  url 'https://github.com/amiaopensource/ltopers/archive/v0.0.9.zip'
-  sha256 '93b792bb826abc04b2bf15431c31dfa502e053867cc1da948f70542728bf9e49'
+  url 'https://github.com/amiaopensource/ltopers/archive/v0.1.0.zip'
+  sha256 '0a1828aa3d9771ead0e6d5212d13392635fe9450c94537b46efb69c0ce201247'
   revision 1
   
   head 'git://github.com/amiaopensource/ltopers.git'
@@ -19,5 +19,6 @@ class Ltopers < Formula
     bin.install "writelto"
     bin.install "renameschemas"
     bin.install "indexschemas"
+    bin.install "collectionchecksum"
   end
 end
