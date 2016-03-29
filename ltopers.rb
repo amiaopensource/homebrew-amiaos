9c30a58cd76cb8eac263acdebc79c6d1b85f1004
Dinah <dinahrhandel@gmail.com>
2016-03-29 11:18:50 -0400

    Update ltopers.rb

--- ltopers.rb
+++ ltopers.rb
@@ -4,7 +4,7 @@ class Ltopers < Formula
   homepage 'https://github.com/amiaopensource/ltopers'
   url 'https://github.com/amiaopensource/ltopers/archive/v0.1.0.zip'
   sha256 '0a1828aa3d9771ead0e6d5212d13392635fe9450c94537b46efb69c0ce201247'
-  revision 1
+  revision 2
   
   head 'git://github.com/amiaopensource/ltopers.git'
 
