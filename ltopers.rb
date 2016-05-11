f98f2175d6acc01533fcd3e3547aa23c29a2ca1a
Dinah <dinahrhandel@gmail.com>
2016-05-11 16:00:08 -0400

    Update ltopers.rb

--- ltopers.rb
+++ ltopers.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ltopers < Formula
   homepage 'https://github.com/amiaopensource/ltopers'
-  url 'https://github.com/amiaopensource/ltopers/archive/v0.1.0.zip'
+  url 'https://github.com/amiaopensource/ltopers/archive/v0.1.1.zip'
   sha256 'fb18b21e12ec1f312e1d9c5c1b62845970cd7cd8fb60c43a8c0befdb2d8b0eda'
   
   head 'git://github.com/amiaopensource/ltopers.git'
