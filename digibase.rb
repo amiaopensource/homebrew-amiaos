becc00e5d625096264761f557a0666a339a7bf5c
Jonathan Farbowitz <jfarbowitz@gmail.com>
2016-05-16 17:58:37 -0400

    Added URL and SHA

--- digibase.rb
+++ digibase.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Digibase < Formula
   homepage 'https://github.com/amiaopensource/digibase'
-  url "https://github.com/amiaopensource/vrecord/archive/v0.7.12.zip"
-  sha256 "07356f53087f5cec35d5a291dd1fa2e23e73f9b0d10d0d54302da95d941cdc71" 
+  url "https://github.com/amiaopensource/digibase/archive/v0.1-beta.zip"
+  sha256 "417f06922de26dac8fcef7c4770034064add901d" 
   head 'https://github.com/amiaopensource/digibase.git'
 
   depends_on "mysql"
