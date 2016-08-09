2d5d65b4716daa6329e903e696529ef694c43892
Andrew Weaver <weevz@uw.edu>
2016-08-09 16:06:58 -0400

    Update vrecord.rb

--- vrecord.rb
+++ vrecord.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vrecord < Formula
   homepage 'https://github.com/amiaopensource/vrecord'
-  url "https://github.com/amiaopensource/vrecord/archive/v0.7.15.zip"
-  sha256 "d036c3ed81426a6ca43989b2115e473850ecdc0bec81e3f5385975f1ab40945b" 
+  url "https://github.com/amiaopensource/vrecord/archive/v0.7.16.zip"
+  sha256 "e9be751bfac6e07671d737f84fb28efedc73f1f21e2c16794e1c376a156b2ae1" 
   head 'https://github.com/amiaopensource/vrecord.git'
 
   depends_on "sdl"
