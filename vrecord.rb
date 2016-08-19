e7879e72408ea839fb958be2b0e03ac6d923caa3
Andrew Weaver <weevz@uw.edu>
2016-08-19 14:28:28 -0400

    removed caskroom/cask/brew-cask as dependency

--- vrecord.rb
+++ vrecord.rb
@@ -5,13 +5,13 @@ class Vrecord < Formula
   url "https://github.com/amiaopensource/vrecord/archive/v0.7.17.zip"
   sha256 "e198c20e623b148c2cab0188fd6749c2cb63f312f3b1e2a608e3954aa1d60fee" 
   head 'https://github.com/amiaopensource/vrecord.git'
+  revision 1
 
   depends_on "sdl"
   depends_on "cowsay"
   depends_on "ffmpeg" => [ 'with-ffplay', 'with-freetype' ]
   depends_on 'amiaopensource/amiaos/bmdtools'
   depends_on "xmlstarlet" => :recommended
-  depends_on "caskroom/cask/brew-cask"
 
   def install
      bin.install 'vrecord'
