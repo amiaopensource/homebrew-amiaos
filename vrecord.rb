17e70563f3447afe122acfe21a819c2f96fb10d5
Dave Rice <dave@dericed.com>
2016-10-14 17:03:26 -0400

    replace --with-ffplay with --with-sdl
    fixes https://github.com/amiaopensource/vrecord/issues/76 so now @bturkus can deal

--- vrecord.rb
+++ vrecord.rb
@@ -5,11 +5,11 @@ class Vrecord < Formula
   url "https://github.com/amiaopensource/vrecord/archive/v0.7.22.zip"
   sha256 "5418125c1e6ccaf70bd2a8239a059dc147272061a1e5d8f4a104a964ea51f017" 
   head 'https://github.com/amiaopensource/vrecord.git'
-
+  revision 1
 
   depends_on "sdl"
   depends_on "cowsay"
-  depends_on "ffmpeg" => [ 'with-ffplay', 'with-freetype' ]
+  depends_on "ffmpeg" => [ 'with-sdl', 'with-freetype' ]
   depends_on 'amiaopensource/amiaos/bmdtools'
   depends_on "xmlstarlet" => :recommended
   depends_on "mpv"
