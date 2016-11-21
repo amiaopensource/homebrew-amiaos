16f34cb14db76fde20e3ba17ff9d82b935f7adb1
Dave Rice <dave@dericed.com>
2016-11-21 12:39:16 -0500

    add xsl needed by prot2rs and mysql dependency

--- pbprotracktor.rb
+++ pbprotracktor.rb
@@ -5,11 +5,13 @@ class Pbprotracktor < Formula
   url "https://github.com/amiaopensource/pbprotracktor/archive/v20161121.tar.gz"
   sha256 "8c4f1c368b5af5917c8462e807d9fded9062b2abd398e74da4910a136fe207fe" 
   head 'https://github.com/amiaopensource/pbprotracktor.git'
-  revision 1
+  revision 2
 
+  depends_on "mysql"
   depends_on "postgresql"
 
   def install
+     bin.install 'pbcore2resourcespace.xsl'
      bin.install 'psql2pbcore.xsl'
      bin.install 'pbprotracktor'
      bin.install 'prot2rs'
