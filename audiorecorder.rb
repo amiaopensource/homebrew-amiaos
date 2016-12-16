383bea6eec8a5b58f347e42dfe4bd8d3c1501262
Andrew Weaver <weevz@uw.edu>
2016-12-16 13:38:47 -0500

    integrate BWF Metaedit/Cask install of Pashua

--- audiorecorder.rb
+++ audiorecorder.rb
@@ -2,15 +2,32 @@ require 'formula'
 
 class Audiorecorder < Formula
   homepage 'https://github.com/amiaopensource/audiorecorder'
-  url "https://github.com/amiaopensource/audiorecorder/archive/0.0.00.zip"
-  sha256 "1d875f38a25eca929ca40f688e8e59a4f0fb2bf2eb2bff56b7cdf500828fae21" 
+  url "https://github.com/amiaopensource/audiorecorder/archive/0.0.01.zip"
+  sha256 "c2b886f7d5d0db9b32551623e1319324db396888f68191ab4b74e234c4ed2690" 
   
   depends_on "sdl"
   depends_on "ffmpeg" => [ 'with-sdl2', 'with-freetype' ]
   depends_on "mpv"
 
   def install
-     bin.install 'audiorecorder'
-     prefix.install 'Pashua.app'
+    bin.install 'audiorecorder'
+    if File.exist? File.expand_path '/usr/local/bin/bwfmetaedit'
+      puts 'BWF Metaedit confirmed'
+    else
+      bin.install 'bwfmetaedit'
+    end
+  end
+  def post_install;
+    puts "Checking for dependency Pashua. If Pashua is not found will attempt to install"
+      if File.exist? File.expand_path '/Applications/Pashua.app'
+        puts 'Pashua was found'
+      else
+        if `brew cask ls --versions pashua 2>&1 | head -n1`.include? 'Warning: pashua is not installed'
+          puts 'installing Pashua'
+          system `echo 'brew cask install pashua > /dev/null'`
+        else
+          puts 'Pashua was found'
+        end
+      end
   end
 end
