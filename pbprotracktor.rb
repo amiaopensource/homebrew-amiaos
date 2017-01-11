3d5eb9bdfad0c7bf06190feac7445d366ae0e6c5
Dave Rice <dave@dericed.com>
2017-01-11 16:37:29 -0500

    pbprotracktor v20170111

--- pbprotracktor.rb
+++ pbprotracktor.rb
@@ -2,17 +2,15 @@ require 'formula'
 
 class Pbprotracktor < Formula
   homepage 'https://github.com/amiaopensource/pbprotracktor'
-  url "https://github.com/amiaopensource/pbprotracktor/archive/v20161121.tar.gz"
-  sha256 "8c4f1c368b5af5917c8462e807d9fded9062b2abd398e74da4910a136fe207fe" 
+  url "https://github.com/amiaopensource/pbprotracktor/archive/v20170111.tar.gz"
+  sha256 "5f6082407ca9e771a39193034b78b98af89c189daa757421ff4b8f4b8c133461" 
   head 'https://github.com/amiaopensource/pbprotracktor.git'
-  revision 2
 
   depends_on "mysql"
   depends_on "postgresql"
 
   def install
      bin.install 'pbcore2resourcespace.xsl'
-     bin.install 'psql2pbcore.xsl'
      bin.install 'pbprotracktor'
      bin.install 'prot2rs'
   end
