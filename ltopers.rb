abc65b453127bc20c3bcc5c8a2894a9c37dc81ee
Andrew Weaver <weevz@uw.edu>
2017-02-07 16:16:06 -0500

    Update ltopers.rb

--- ltopers.rb
+++ ltopers.rb
@@ -4,9 +4,8 @@ class Ltopers < Formula
   homepage 'https://github.com/amiaopensource/ltopers'
   url 'https://github.com/amiaopensource/ltopers/archive/v0.1.6.tar.gz'
   sha256 'd12404d9dd07f5027b47c23902bf83baf28572972c206f99ad1ace6b480e972c'
-  revision 1
-  
   head 'git://github.com/amiaopensource/ltopers.git'
+  revision 2
 
   depends_on 'coreutils'
   depends_on 'xmlstarlet'
