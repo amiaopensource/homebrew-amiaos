934a616997403a1f0d15c0db13bc4bb10b179b07
Andrew Weaver <weevz@uw.edu>
2017-02-15 09:07:51 -0500

    Update audiorecorder.rb

--- audiorecorder.rb
+++ audiorecorder.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Audiorecorder < Formula
   homepage 'https://github.com/amiaopensource/audiorecorder'
-  url "https://github.com/amiaopensource/audiorecorder/archive/0.0.03.tar.gz"
-  sha256 "9741683835f9da6f3f05e346b826d93aba21384c3ed72df8fa07b47a957029e1"
+  url "https://github.com/amiaopensource/audiorecorder/archive/0.0.04.tar.gz"
+  sha256 "b16aa7e17b3ae6df58a64d92a30b210c8e5c0f8edf4badb410743bbfceee6129"
   
   depends_on "sdl"
   depends_on "ffmpeg" => [ 'with-sdl2', 'with-freetype' ]
