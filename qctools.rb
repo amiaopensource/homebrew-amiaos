fd21a2fdc41d25e8fb9b724f0b2899ebe9127308
Dave Rice <dave@dericed.com>
2017-04-11 11:35:13 -0400

    yay qt5 is now qt

--- qctools.rb
+++ qctools.rb
@@ -7,7 +7,7 @@ class Qctools < Formula
 
   depends_on "pkg-config" => :build
   depends_on "amiaopensource/amiaos/qwt-qt5"
-  depends_on "qt5"
+  depends_on "qt"
   depends_on "decklinksdk"
   depends_on "ffmpeg"
   depends_on "freetype"
@@ -16,9 +16,9 @@ class Qctools < Formula
     ENV["QCTOOLS_USE_BREW"]="true"
     path = ENV["PATH"]
     ENV["PATH"] = "#{path}:#{HOMEBREW_PREFIX}/bin"
-        
+
     cd "Project/QtCreator" do
-      system "#{Formula["qt5"].bin}/qmake", "qctools.pro"
+      system "qmake", "qctools.pro"
       system "make"
       prefix.install "qctools-gui/QCTools.app"
     end
