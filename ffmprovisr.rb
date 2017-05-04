fbc443c0f9d85a7a5ec1a73c985ed043102a01e7
Andrew Weaver <weevz@uw.edu>
2017-05-04 14:20:09 -0400

    add ffmprovisr

---
+++ ffmprovisr.rb
@@ -0,0 +1,11 @@
+class Ffmprovisr < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/amiaopensource/ffmprovisr/archive/v2017-04-19.tar.gz"
+  sha256 "4120fb650d35ab8fa8b96e6ec78740b3ade27249dd4193d858ffb1956a6098dc"
+
+  def install
+  bin.install 'scripts/ffmprovisr'
+  prefix.install 'css', 'img', 'js', 'scripts', 'code_of_conduct.md', 'index.html', 'readme.md'
+  end
+end
