af59bda99aaa7768f985c60f7cffa7c078345ade
Andrew Weaver <weevz@uw.edu>
2017-05-04 14:21:31 -0400

    spacing

--- ffmprovisr.rb
+++ ffmprovisr.rb
@@ -5,7 +5,7 @@ class Ffmprovisr < Formula
   sha256 "4120fb650d35ab8fa8b96e6ec78740b3ade27249dd4193d858ffb1956a6098dc"
 
   def install
-  bin.install 'scripts/ffmprovisr'
-  prefix.install 'css', 'img', 'js', 'scripts', 'code_of_conduct.md', 'index.html', 'readme.md'
+    bin.install 'scripts/ffmprovisr'
+    prefix.install 'css', 'img', 'js', 'scripts', 'code_of_conduct.md', 'index.html', 'readme.md'
   end
 end
