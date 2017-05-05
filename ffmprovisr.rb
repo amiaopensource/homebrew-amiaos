01068e133fbf3b3f07e2adc5aea0859d30ffcba9
Andrew Weaver <weevz@uw.edu>
2017-05-05 11:52:06 -0400

    add homepage

--- ffmprovisr.rb
+++ ffmprovisr.rb
@@ -1,6 +1,6 @@
 class Ffmprovisr < Formula
   desc ""
-  homepage ""
+  homepage "https://github.com/amiaopensource/ffmprovisr"
   url "https://github.com/amiaopensource/ffmprovisr/archive/v2017-05-05.tar.gz"
   sha256 "914952725bcf481174ffb1b7c2915eede7d2162de4e2c6d884faf4e10b8f93f8"
 
