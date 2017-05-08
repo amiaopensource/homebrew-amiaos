25d62f4173bd5285241b9217308bca466c84f484
Ethan Gates <ethan.gates@nyu.edu>
2017-05-08 17:24:43 -0400

    update to v2017-05-08 release
    formula needs a release version that includes "cable-bible" shell script to install properly

--- cable-bible.rb
+++ cable-bible.rb
@@ -1,8 +1,8 @@
 class CableBible < Formula
   desc "A comprehensive guide to cables and connectors used in audiovisual archiving"
   homepage "https://github.com/amiaopensource/cable-bible"
-  url "https://github.com/amiaopensource/cable-bible/archive/v2017-02-27.tar.gz"
-  sha256 "9fd5a792dc0973721abe08963775072462ce5ee0f1c115b6cfb7ca38c8420902"
+  url "https://github.com/amiaopensource/cable-bible/archive/v2017-05-08.tar.gz"
+  sha256 "c052e49914a7e0d27a76a3b8d2394f7a3a733a5e7126c5006032d3f3082a4f8d"
 
   def install
     bin.install "cable-bible"
