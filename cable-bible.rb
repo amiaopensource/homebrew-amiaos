82d3cfd77248ba4bc5b0f7d70fd0bbd4054d71ae
Ethan Gates <ethan.gates@nyu.edu>
2017-05-08 17:04:59 -0400

    style edits
    indents and removal of empty lines

--- cable-bible.rb
+++ cable-bible.rb
@@ -5,8 +5,7 @@ class CableBible < Formula
   sha256 "9fd5a792dc0973721abe08963775072462ce5ee0f1c115b6cfb7ca38c8420902"
 
   def install
-  bin.install "cable-bible"
-  prefix.install "css", "images", "js", "README.md", "code_of_conduct.md", "index.html", "params.json", "pinouts.md"
+    bin.install "cable-bible"
+    prefix.install "css", "images", "js", "README.md", "code_of_conduct.md", "index.html", "params.json", "pinouts.md"
   end
-
 end
