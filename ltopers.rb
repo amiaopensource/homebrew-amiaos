859e5d5331945530b7c11b6fce4809c58ff8825e
Reto Kromer <retokromer@users.noreply.github.com>
2017-05-18 17:32:24 +0200

    update ltopers

--- ltopers.rb
+++ ltopers.rb
@@ -1,25 +1,39 @@
 class Ltopers < Formula
   desc "Scripts for doing stuff with LTFS"
   homepage "https://github.com/amiaopensource/ltopers"
-  url "https://github.com/amiaopensource/ltopers/archive/v0.1.16.tar.gz"
-  sha256 "8cc7a05894f34936d59bf6c3f9917f44bc7639bf3855ab151c2a4c7c2c4d2f8a"
+  url "https://github.com/amiaopensource/ltopers/archive/v2017-05-20.tar.gz"
+  sha256 ""
   head "https://github.com/amiaopensource/ltopers.git"
+  version "2017-05-20"
+  
+  option "--with-db", "Build database-related commands"
+  option "--with-cuny", "Build database-related and CUNY-specific commands"
 
   depends_on "coreutils"
-  depends_on "xmlstarlet"
   depends_on "mediamicroservices/mm/mm"
+  depends_on "xmlstarlet"
 
   def install
+    bin.install "ltopers"
     bin.install "formatlto"
     bin.install "mountlto"
+    bin.install "readlto"
     bin.install "writelto"
+    bin.install "verifylto"
+  end
+  
+  if build.with? "db"
+    bin.install "ingestcollectionchecksum"
+    bin.install "ingestschemas"
+    bin.install "searchlto"    
+  end
+
+  if build.with? "cuny"
+    bin.install "ingestcollectionchecksum"
+    bin.install "ingestschemas"
+    bin.install "searchlto"    
     bin.install "renameschemas"
     bin.install "indexschemas"
     bin.install "collectionchecksum"
-    bin.install "verifylto"
-    bin.install "ingestschemas"
-    bin.install "searchlto"
-    bin.install "ingestcollectionchecksum"
-    bin.install "readlto"
   end
 end
