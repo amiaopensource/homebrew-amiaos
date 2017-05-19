1014354db609b5b1cc9955fa4a02324561130ab8
Reto Kromer <retokromer@users.noreply.github.com>
2017-05-19 08:44:32 +0200

    new version

--- ltopers.rb
+++ ltopers.rb
@@ -6,8 +6,8 @@ class Ltopers < Formula
   head "https://github.com/amiaopensource/ltopers.git"
   version "2017-05-20"
   
-  option "--with-db", "Build database-related commands"
-  option "--with-cuny", "Build database-related and CUNY-specific commands"
+  option "with-db", "Build database-related commands"
+  option "with-cuny", "Build database-related and CUNY-specific commands"
 
   depends_on "coreutils"
   depends_on "mediamicroservices/mm/mm"
@@ -20,20 +20,20 @@ class Ltopers < Formula
     bin.install "readlto"
     bin.install "writelto"
     bin.install "verifylto"
-  end
-  
-  if build.with? "db"
-    bin.install "ingestcollectionchecksum"
-    bin.install "ingestschemas"
-    bin.install "searchlto"    
-  end
-
-  if build.with? "cuny"
-    bin.install "ingestcollectionchecksum"
-    bin.install "ingestschemas"
-    bin.install "searchlto"    
-    bin.install "renameschemas"
-    bin.install "indexschemas"
-    bin.install "collectionchecksum"
+ 
+    if build.with? "db"
+      bin.install "ingestcollectionchecksum"
+      bin.install "ingestschemas"
+      bin.install "searchlto"
+    end
+    
+    if build.with? "cuny"
+      bin.install "ingestcollectionchecksum"
+      bin.install "ingestschemas"
+      bin.install "searchlto"
+      bin.install "renameschemas"
+      bin.install "indexschemas"
+      bin.install "collectionchecksum"
+    end
   end
 end
