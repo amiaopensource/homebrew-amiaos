d284f49dd0f48eb806c74d889c1df0d43bed9df9
Reto Kromer <retokromer@users.noreply.github.com>
2017-05-20 06:51:24 +0200

    add SHA256

--- ltopers.rb
+++ ltopers.rb
@@ -2,7 +2,7 @@ class Ltopers < Formula
   desc "Scripts for doing stuff with LTFS"
   homepage "https://github.com/amiaopensource/ltopers"
   url "https://github.com/amiaopensource/ltopers/archive/v2017-05-20.tar.gz"
-  sha256 ""
+  sha256 "85323514cf12de4a3eea2883963242371986654c473fc26fb99efe9383a2361e"
   head "https://github.com/amiaopensource/ltopers.git"
   version "2017-05-20"
   
