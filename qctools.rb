b331ad57a4d6f07a8d640c03934d741514b4c135
Reto Kromer <retokromer@users.noreply.github.com>
2017-06-04 06:40:35 +0200

    update QCTools formula (#45)

--- qctools.rb
+++ qctools.rb
@@ -4,28 +4,24 @@ class Qctools < Formula
   url "https://github.com/bavc/qctools/archive/v0.8.tar.gz"
   sha256 "5362dc8325aeb37e0742a5e5df7b831e7fe82a7b06c72c50463a43a7ad0b56bc"
   head "https://github.com/bavc/qctools.git"
-  revision 2
+  revision 3
 
   depends_on "pkg-config" => :build
   depends_on "amiaopensource/amiaos/qwt-qt5"
   depends_on "qt"
-  depends_on "freetype"
   depends_on "ffmpeg" => ["with-freetype"]
 
   def install
     ENV["QCTOOLS_USE_BREW"]="true"
-    path = ENV["PATH"]
-    ENV["PATH"] = "#{path}:#{HOMEBREW_PREFIX}/bin"
 
-    cd "Project/QtCreator" do
-      cd "qctools-lib"
+    cd "Project/QtCreator/qctools-lib" do
       system "qmake", "qctools-lib.pro"
       system "make"
-      cd "../qctools-gui"
+    end
+    cd "Project/QtCreator/qctools-gui" do
       system "qmake", "qctools-gui.pro"
       system "make"
-      cd ".."
-      prefix.install "qctools-gui/QCTools.app"
+      prefix.install "QCTools.app"
     end
   end
 end
