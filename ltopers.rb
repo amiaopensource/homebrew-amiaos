685c0e679b7e5fae929bb6b434278c59ca4ee846
Reto Kromer <retokromer@users.noreply.github.com>
2017-07-14 17:10:11 +0200

    v2017-07-15

--- ltopers.rb
+++ ltopers.rb
@@ -1,10 +1,12 @@
 class Ltopers < Formula
   desc "Scripts for doing stuff with LTFS"
   homepage "https://github.com/amiaopensource/ltopers"
-  url "https://github.com/amiaopensource/ltopers/archive/v2017-06-02.tar.gz"
+  url "https://github.com/amiaopensource/ltopers/archive/v2017-07-15.tar.gz"
   sha256 "fea230ea7d779693a637bb5fbc5b6b5042f7fe20b71d99ac40ef7550b675e338"
-  version "2017-06-02"
+  version "2017-07-15"
   head "https://github.com/amiaopensource/ltopers.git"
+
+  bottle :unneeded
   
   option "with-db", "Build database-related commands"
   option "with-cuny", "Build database-related and CUNY-specific commands"
