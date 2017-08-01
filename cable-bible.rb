9553943a2585f7ceca7c62afe6de5689a140727d
Reto Kromer <retokromer@users.noreply.github.com>
2017-08-01 15:35:52 +0200

    cable-bible version 2017-08-01

--- cable-bible.rb
+++ cable-bible.rb
@@ -1,9 +1,9 @@
 class CableBible < Formula
   desc "A comprehensive guide to cables and connectors used in audiovisual archiving"
   homepage "https://github.com/amiaopensource/cable-bible"
-  url "https://github.com/amiaopensource/cable-bible/archive/v2017-07-29.tar.gz"
-  version "2017-07-29"
-  sha256 "f7f7f1bc7ce2b3a1859035fdfc8ce9a228cfbe02958a2550e614458f586a100e"
+  url "https://github.com/amiaopensource/cable-bible/archive/v2017-08-01.tar.gz"
+  version "2017-08-01"
+  sha256 "ff4c19f3ec3468bd592f4eb800b159d7cfdc9a80b7714ed5578247dea620480e"
 
   bottle :unneeded
 
