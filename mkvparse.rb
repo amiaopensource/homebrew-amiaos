47ea89447b89f101e998472de082ce11e29128f1
Reto Kromer <retokromer@users.noreply.github.com>
2017-08-21 17:52:34 +0200

    WIP

--- mkvparse.rb
+++ mkvparse.rb
@@ -1,10 +1,11 @@
 class Mkvparse < Formula
-  homepage 'https://github.com/vi/mkvparse'
-  head 'https://github.com/vi/mkvparse.git'
- 
+  desc "Simple Matroska (MKV) parser"
+  homepage "https://github.com/vi/mkvparse"
+  head "https://github.com/vi/mkvparse.git"
+
   def install
-     bin.install 'mkv2xml'
-     bin.install 'xml2mkv'
-     bin.install 'mkvparse.py'
+    bin.install "mkv2xml"
+    bin.install "xml2mkv"
+    bin.install "mkvparse.py"
   end
 end
