4b1ea465790f3645cea5578f6c1a6a9308209f1c
Reto Kromer <retokromer@users.noreply.github.com>
2017-08-22 08:05:33 +0200

    Update mkvparse.rb

--- mkvparse.rb
+++ mkvparse.rb
@@ -1,6 +1,8 @@
 class Mkvparse < Formula
   desc "Simple Matroska (MKV) parser"
   homepage "https://github.com/vi/mkvparse"
+  url "https://github.com/vi/mkvparse/archive/v1.1.0.tar.gz"
+  sha256 "9884666af60cdae4fc5f1bec0ef6fd4740827d15b09a4c346b7828c5cf36c1cf"
   head "https://github.com/vi/mkvparse.git"
 
   def install
