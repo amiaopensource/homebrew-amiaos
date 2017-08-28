83b3dd6b264e5ce9d65c51899a0a9e39b2df3b34
Reto Kromer <retokromer@users.noreply.github.com>
2017-08-28 17:16:39 +0200

    v2017-08-28

--- pbprotracktor.rb
+++ pbprotracktor.rb
@@ -1,9 +1,10 @@
 class Pbprotracktor < Formula
-  homepage 'https://github.com/amiaopensource/pbprotracktor'
-  url "https://github.com/amiaopensource/pbprotracktor/archive/v2017-06-16.tar.gz"
-  version "2017-06-16"
-  sha256 "7d170947ce83964923873322542d383c3ee4d7927363bd4246b9d931468e3936" 
-  head 'https://github.com/amiaopensource/pbprotracktor.git'
+  desc "Postgres calls to databases to produce a PBCore XML output"
+  homepage "https://github.com/amiaopensource/pbprotracktor"
+  url "https://github.com/amiaopensource/pbprotracktor/archive/v2017-08-28.tar.gz"
+  version "2017-08-26"
+  sha256 "0ba6eeb18247cd3b434309ac2c4d4ba38f05b3ca2936367e6f90bf177acffb0b"
+  head "https://github.com/amiaopensource/pbprotracktor.git"
 
   bottle :unneeded
 
@@ -11,9 +12,9 @@ class Pbprotracktor < Formula
   depends_on "postgresql"
 
   def install
-     bin.install 'pbprotracktor'
-     bin.install 'prot2rs'
-     bin.install 'rs_search'
-     bin.install 'rs_uploadfile'
+    bin.install "pbprotracktor"
+    bin.install "prot2rs"
+    bin.install "rs_search"
+    bin.install "rs_uploadfile"
   end
 end
