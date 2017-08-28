758fd1c79db9ca6290f733f0e111b2ce6ba97711
Reto Kromer <retokromer@users.noreply.github.com>
2017-08-28 17:19:54 +0200

    pbprotracktor v2017-08-28

--- pbprotracktor.rb
+++ pbprotracktor.rb
@@ -2,7 +2,7 @@ class Pbprotracktor < Formula
   desc "Postgres calls to databases to produce a PBCore XML output"
   homepage "https://github.com/amiaopensource/pbprotracktor"
   url "https://github.com/amiaopensource/pbprotracktor/archive/v2017-08-28.tar.gz"
-  version "2017-08-26"
+  version "2017-08-28"
   sha256 "0ba6eeb18247cd3b434309ac2c4d4ba38f05b3ca2936367e6f90bf177acffb0b"
   head "https://github.com/amiaopensource/pbprotracktor.git"
 
