0b116192e192fd86942169b7aa6d0d1a557b1688
Reto Kromer <retokromer@users.noreply.github.com>
2017-10-01 07:43:35 +0200

    audiorecorder v0.1.05

--- audiorecorder.rb
+++ audiorecorder.rb
@@ -1,10 +1,9 @@
 class Audiorecorder < Formula
   desc "Tool for calibration and recording of analog audio sources"
   homepage "https://github.com/amiaopensource/audiorecorder"
-  url "https://github.com/amiaopensource/audiorecorder/archive/0.1.04.tar.gz"
-  sha256 "5975d4b6adb312a1c37fb0322560c57f0dba171d83df08a2890ce2fd212a023c"
+  url "https://github.com/amiaopensource/audiorecorder/archive/0.1.05.tar.gz"
+  sha256 "0da690035c1e75dac005f63f9e1035355eae7c981eda6cb601000855b5606a84"
   head "https://github.com/amiaopensource/audiorecorder.git"
-  revision 1
 
   depends_on "sdl"
   depends_on "ffmpeg" => ["with-sdl2", "with-freetype"]
