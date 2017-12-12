44426b08e504a97e5c45a39b735507034ff01bc4
Reto Kromer <retokromer@users.noreply.github.com>
2017-12-12 16:39:33 +0100

    digibase version 0.2-beta

--- digibase.rb
+++ digibase.rb
@@ -1,15 +1,16 @@
 class Digibase < Formula
-  homepage 'https://github.com/amiaopensource/digibase'
-  url "https://github.com/amiaopensource/digibase/archive/v0.1-beta.zip"
-  sha256 "e69c23135e188bf7a2e71aa81fd1a9993131d0d1a239f879206eada302f28207" 
-  head 'https://github.com/amiaopensource/digibase.git'
+  desc "MySQL to create a DB tracking tapes through a digitization workflow"
+  homepage "https://github.com/amiaopensource/digibase"
+  url "https://github.com/amiaopensource/digibase/archive/v0.2-beta.tar.gz"
+  sha256 "bba6b7da1d3dca0d0da8efc9cf992e0bc21b2769c6aeb611e461715542341746"
+  head "https://github.com/amiaopensource/digibase.git"
 
   depends_on "mysql"
 
   def install
-     bin.install 'setuptables'
-     bin.install 'check_db'
-     bin.install 'finish_qc'
-     bin.install 'digibase_functions'
+    bin.install "setuptables"
+    bin.install "check_db"
+    bin.install "finish_qc"
+    bin.install "digibase_functions"
   end
 end
