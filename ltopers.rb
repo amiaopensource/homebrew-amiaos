0e042637ea2672b5112c0b7c3900b124fa7b9694
Reto Kromer <retokromer@users.noreply.github.com>
2018-01-25 10:55:16 +0100

    LTOpers version 2018–01–25

--- ltopers.rb
+++ ltopers.rb
@@ -1,9 +1,9 @@
 class Ltopers < Formula
   desc "Scripts for doing stuff with LTFS"
   homepage "https://github.com/amiaopensource/ltopers"
-  url "https://github.com/amiaopensource/ltopers/archive/v2018-01-13.tar.gz"
-  version "2018-01-13"
-  sha256 "ed3fc337769845894cc6788652cfaa881a8d75eb15d4fb19e607de56289ad84c"
+  url "https://github.com/amiaopensource/ltopers/archive/v2018-01-25.tar.gz"
+  version "2018-01-25"
+  sha256 "604cf350ed6a9a780010e6423b87be996d417a8077f7ef032ae491acc7197e94"
   head "https://github.com/amiaopensource/ltopers.git"
 
   bottle :unneeded
