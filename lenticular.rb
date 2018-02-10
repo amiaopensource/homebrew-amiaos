5357aa5ca3184b9214880b6f54dd07e6ec42be4b
Reto Kromer <info@reto.ch>
2018-02-10 17:47:19 +0100

    lenticular version 2018-02-11
    first alpha for testing purpose

--- lenticular.rb
+++ lenticular.rb
@@ -1,9 +1,9 @@
 class Lenticular < Formula
-  desc "Digital Reconstruction of Lenticular Colours"
+  desc "Digital Restoration of Lenticular Colours"
   homepage "https://github.com/amiaopensource/lenticular"
-  url "https://github.com/amiaopensource/lenticular/archive/v2018-02-10.tar.gz"
-  version "2018-02-10"
-  sha256 "d53caf0cbb1e55bb399345a5714898e1e4a491c9130c1eb0774b89447588fa18"
+  url "https://github.com/amiaopensource/lenticular/archive/v2018-02-11.tar.gz"
+  version "2018-02-11"
+  sha256 "28170280b029debf36e0a941b4a043431f418db3388710ab2aa3005268a869e0"
   head "https://github.com/amiaopensource/lenticular.git"
 
   def install
