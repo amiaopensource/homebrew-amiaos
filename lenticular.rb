7c7b942c6b4d317f1659b005e508df255defed05
Reto Kromer <info@reto.ch>
2018-02-16 14:22:38 +0100

    lenticular 2018-02-16 alpha

--- lenticular.rb
+++ lenticular.rb
@@ -1,9 +1,9 @@
 class Lenticular < Formula
   desc "Digital Restoration of Lenticular Colours"
   homepage "https://github.com/amiaopensource/lenticular"
-  url "https://github.com/amiaopensource/lenticular/archive/v2018-02-12.tar.gz"
-  version "2018-02-12"
-  sha256 "65696d8d859a4ae7fd3818d02b145937ba44f7809c45f0cdaed2a4577b9eb8c4"
+  url "https://github.com/amiaopensource/lenticular/archive/v2018-02-16.tar.gz"
+  version "2018-02-16"
+  sha256 "f9c06d594ba65bc49af1aa09f0691a789a7f0a27c8512388082bc26e8f8ed4d8"
   head "https://github.com/amiaopensource/lenticular.git"
 
   option "with-frameCropper", "Add the 'frameCropper' tool"
