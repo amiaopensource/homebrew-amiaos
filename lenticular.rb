c93d4af2a70c75f1a6d82b47c31fa68bdbcf2a16
Reto Kromer <info@reto.ch>
2018-03-03 12:55:41 +0100

    lenticular v2018-03-04

--- lenticular.rb
+++ lenticular.rb
@@ -1,20 +1,20 @@
 class Lenticular < Formula
   desc "Digital Restoration of Lenticular Colours"
   homepage "https://github.com/amiaopensource/lenticular"
-  url "https://github.com/amiaopensource/lenticular/archive/v2018-02-24.tar.gz"
-  version "2018-02-24"
-  sha256 "61b85d6a98c86f417dad0ee7977d6bdb4c0d07b9fe8a24707424a9f533e6fee4"
+  url "https://github.com/amiaopensource/lenticular/archive/v2018-03-04.tar.gz"
+  version "2018-03-04"
+  sha256 "bfa7e0b1492684a9537966c078a2c775c16cb31325498f8911d5f126a6f3cbe2"
   head "https://github.com/amiaopensource/lenticular.git"
 
-  option "with-frameCropper", "Install 'frameCropper' tool"
-  option "with-inStudy", "Install 'inStudy' tool"
+  option "with-framecropper", "Install frame-cropper tool"
+  option "with-interpolationstudy", "Install interpolation-study tool"
 
   def install
     cd "lenticular" do
       system "make"
       bin.install "lenticular"
-      bin.install "frameCropper" if build.with? "frameCropper"
-      bin.install "inStudy" if build.with? "inStudy"
+      bin.install "framecropper" if build.with? "framecropper"
+      bin.install "interpolationstudy" if build.with? "interpolationstudy"
       man1.install "lenticular.1"
     end
   end
