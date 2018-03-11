586daf5383d5cf6f150390773dfdd632f500f103
Reto Kromer <info@reto.ch>
2018-03-11 16:59:35 +0100

    add depends_on "libtiff"

--- lenticular.rb
+++ lenticular.rb
@@ -4,10 +4,13 @@ class Lenticular < Formula
   url "https://github.com/amiaopensource/lenticular/archive/v2018-03-04.tar.gz"
   version "2018-03-04"
   sha256 "bfa7e0b1492684a9537966c078a2c775c16cb31325498f8911d5f126a6f3cbe2"
+  revision 1
   head "https://github.com/amiaopensource/lenticular.git"
 
   option "with-framecropper", "Install frame-cropper tool"
   option "with-interpolationstudy", "Install interpolation-study tool"
+
+  depends_on "libtiff"
 
   def install
     cd "lenticular" do
