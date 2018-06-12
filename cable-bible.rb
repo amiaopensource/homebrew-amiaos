595a8a959f1e42c1e132e0efa5c648a2bf5ac5bd
Reto Kromer <retokromer@users.noreply.github.com>
2018-06-12 15:48:22 +0200

    cable-bible version 2018-06-12 (#108)

--- cable-bible.rb
+++ cable-bible.rb
@@ -1,14 +1,14 @@
 class CableBible < Formula
   desc "Guide to cables and connectors used in audiovisual archiving"
   homepage "https://github.com/amiaopensource/cable-bible"
-  url "https://github.com/amiaopensource/cable-bible/archive/v2018-03-03.tar.gz"
-  version "2018-03-03"
-  sha256 "d8db0ab53e7e821650c3e2db997e5b44ef876d49f5557ab0a02431b18a5d68da"
+  url "https://github.com/amiaopensource/cable-bible/archive/v2018-06-12.tar.gz"
+  version "2018-06-12"
+  sha256 "4dfaf433777181fff81fbfd925c1cdca2a0ad04dccf9f8d815fa39cd2a325816"
 
   bottle :unneeded
 
   def install
     bin.install "cable-bible"
-    prefix.install "css", "images", "js", "README.md", "code_of_conduct.md", "index.html", "params.json", "pinouts.md"
+    prefix.install "css", "images", "js", "README.md", "code_of_conduct.md", "index.html", "params.json", "pinouts.html"
   end
 end
