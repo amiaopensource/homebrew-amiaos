169f8b0fb62a6aeb6fb419fe298c55badc8093fc
Ethan Gates <e.gates42@gmail.com>
2018-06-12 09:11:27 -0400

    update version
    - adds just-committed SCSI connectors
    - switches "pinouts.md" to "pinouts.html" to avoid possible local install error

--- cable-bible.rb
+++ cable-bible.rb
@@ -1,14 +1,14 @@
 class CableBible < Formula
   desc "Guide to cables and connectors used in audiovisual archiving"
   homepage "https://github.com/amiaopensource/cable-bible"
-  url "https://github.com/amiaopensource/cable-bible/archive/v2018-06-11.tar.gz"
-  version "2018-06-11"
-  sha256 "3fd5b53118e240e0070902fc31d99a63f023aecee1e3ec1b4d7d1e70703f0177"
+  url "https://github.com/amiaopensource/cable-bible/archive/v2018-06-12.tar.gz"
+  version "2018-06-12"
+  sha256 "4dfaf433777181fff81fbfd925c1cdca2a0ad04dccf9f8d815fa39cd2a325816"
 
   bottle :unneeded
 
   def install
     bin.install "cable-bible"
-    prefix.install "css", "images", "js", "README.md", "code_of_conduct.md", "index.html", "params.json", "pinouts.md"
+    prefix.install "css", "images", "js", "README.md", "code_of_conduct.md", "index.html", "params.json", "pinouts.html"
   end
 end
