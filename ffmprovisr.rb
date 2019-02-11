e12a5f2b6aa231cf315f2958c9d2335fb49b254a
Reto Kromer <retokromer@users.noreply.github.com>
2019-02-11 18:06:31 +0100

    ffmprovisr 2019-02-22

--- ffmprovisr.rb
+++ ffmprovisr.rb
@@ -1,9 +1,9 @@
 class Ffmprovisr < Formula
   desc "Repository of useful FFmpeg command-lines for archivists"
   homepage "https://github.com/amiaopensource/ffmprovisr"
-  url "https://github.com/amiaopensource/ffmprovisr/archive/v2019-01-17.tar.gz"
-  version "2019-01-17"
-  sha256 "5f6034187a157d924ea564d442211b130e404bb66bb9a644ff0b2da91e02658d"
+  url "https://github.com/amiaopensource/ffmprovisr/archive/v2019-02-11.tar.gz"
+  version "2019-02-11"
+  sha256 "f74d58c2baca3e60755baf09e043de6c91d7f2e152dad283643b925f48062d52"
 
   bottle :unneeded
 
