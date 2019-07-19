9a39c834fa4340515bec2a8f2d40d3d7445d479d
Reto Kromer <retokromer@users.noreply.github.com>
2019-07-19 08:15:00 +0200

    error message on macOS

--- ffmpegdecklink.rb
+++ ffmpegdecklink.rb
@@ -9,7 +9,7 @@ class Ffmpegdecklink < Formula
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
   option "with-fdk-aac", "Enable the Fraunhofer FDK AAC library"
   option "with-libass", "Enable ASS/SSA subtitle format"
-  option "with-iec61883", "Enable DV device (Linux)"
+  option "with-iec61883", "Enable DV device (Linux)" if OS.linux?
   option "with-librsvg", "Enable SVG files as inputs via librsvg"
   option "with-libsoxr", "Enable the soxr resample library"
   option "with-libssh", "Enable SFTP protocol via libssh"
