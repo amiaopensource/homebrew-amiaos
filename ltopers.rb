c64e7085800106680f25da8816900ee84ab90018
Reto Kromer <retokromer@users.noreply.github.com>
2020-01-27 18:09:08 +0100

    ltopers versions 2020-01-27

--- ltopers.rb
+++ ltopers.rb
@@ -1,9 +1,9 @@
 class Ltopers < Formula
   desc "Scripts for doing stuff with LTFS"
   homepage "https://github.com/amiaopensource/ltopers"
-  url "https://github.com/amiaopensource/ltopers/archive/v2019-01-22.tar.gz"
-  version "2019-01-22"
-  sha256 "04aa7cbb681d1ce228e3e3ee0bbc1e5460315242feff75a3faebc686455531b5"
+  url "https://github.com/amiaopensource/ltopers/archive/v2020-01-27.tar.gz"
+  version "2020-01-27"
+  sha256 "7d3ba39bab196e352070ce5a4457113bf8682cdbc54469bc16f8f498313781bb"
   head "https://github.com/amiaopensource/ltopers.git"
 
   bottle :unneeded
