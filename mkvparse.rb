4961fb9921e2df31c1c600fe4dca17e04c5bd71b
Reto Kromer <retokromer@users.noreply.github.com>
2020-06-06 06:44:48 +0200

    mkvparse: upgrade to 1.2.0

--- mkvparse.rb
+++ mkvparse.rb
@@ -1,8 +1,8 @@
 class Mkvparse < Formula
   desc "Simple Matroska (MKV) parser"
   homepage "https://github.com/vi/mkvparse"
-  url "https://github.com/vi/mkvparse/archive/v1.1.0.tar.gz"
-  sha256 "9884666af60cdae4fc5f1bec0ef6fd4740827d15b09a4c346b7828c5cf36c1cf"
+  url "https://github.com/vi/mkvparse/archive/v1.2.0.tar.gz"
+  sha256 "951282e3ffade95f5d9951d792578098616c1009264ea7cbd6178f1727ef406d"
   head "https://github.com/vi/mkvparse.git"
 
   def install
