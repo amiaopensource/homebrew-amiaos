6d04798f311bb3e202009f39f69e3afbe5ea6193
Dave Rice <dave@dericed.com>
2020-06-26 15:32:37 -0400

    vrecord v2020-06-26

--- vrecord.rb
+++ vrecord.rb
@@ -1,29 +1,32 @@
 class Vrecord < Formula
   desc "Capturing a video signal and turning it into a digital file"
   homepage "https://github.com/amiaopensource/vrecord"
-  url "https://github.com/amiaopensource/vrecord/archive/v2020-02-10.tar.gz"
-  version "2020-02-10"
-  sha256 "761aa248856eedc23619d41f5902bba8baba1c67bddc322fcaad7cb241a1edfc"
+  url "https://github.com/amiaopensource/vrecord/archive/v2020-06-26.tar.gz"
+  version "2020-06-26"
+  sha256 "e32b77f8da9ece0fe7ed2a0660fc67a1d6ef48ff8a4692161c57223cdaa3412d"
   head "https://github.com/amiaopensource/vrecord.git"
 
   bottle :unneeded
 
-  depends_on "amiaopensource/amiaos/decklinksdk"
+  depends_on "amiaopensource/amiaos/deckcontrol"
   depends_on "amiaopensource/amiaos/ffmpegdecklink"
   depends_on "amiaopensource/amiaos/gtkdialog"
   depends_on "cowsay"
-  depends_on "freetype"
-  depends_on "gnuplot"
-  depends_on "mediaconch"
-  depends_on "mkvtoolnix"
-  depends_on "mpv"
-  depends_on "qcli"
-  depends_on "sdl"
-  depends_on "xmlstarlet"
+
+  on_macos do
+    depends_on "bash"
+    depends_on "gnuplot"
+    depends_on "mediaconch"
+    depends_on "mkvtoolnix"
+    depends_on "mpv"
+    depends_on "qcli"
+    depends_on "xmlstarlet"
+  end
 
   def install
     bin.install "vrecord"
     bin.install "vtest"
+    prefix.install "Resources/audio_mode.gif"
     prefix.install "Resources/qcview.lua"
     prefix.install "Resources/vrecord_policy_ffv1.xml"
     prefix.install "Resources/vrecord_policy_uncompressed.xml"
