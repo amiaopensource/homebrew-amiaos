e7719282381406f5268cabd5712ad3a68cead156
Dave Rice <dave@dericed.com>
2020-06-26 15:28:07 -0400

    use full brew tap path for deckcontrol dependency

--- deckcontrol.rb
+++ deckcontrol.rb
@@ -4,8 +4,9 @@ class Deckcontrol < Formula
   url "https://github.com/bavc/deckcontrol/archive/v0.3.tar.gz"
   sha256 "3fe68d35a29051f81d997d4e28a4775f7b3c7899bdc87d60877c554fd1d4c816"
   head "https://github.com/bavc/deckcontrol.git"
+  revision 1
 
-  depends_on "decklinksdk" => :build
+  depends_on "amiaopensource/amiaos/decklinksdk" => :build
 
   def install
     system "make"
