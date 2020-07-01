dc5c6235979fd9540f307d2cef3b757a2dd4e9ae
Dave Rice <dave@dericed.com>
2020-07-01 13:55:52 -0400

    deckcontrol v0.4 (#249)

--- deckcontrol.rb
+++ deckcontrol.rb
@@ -1,8 +1,8 @@
 class Deckcontrol < Formula
   desc "Control an attached videodeck via Blackmagic Design Decklink SDK"
   homepage "https://github.com/bavc/deckcontrol"
-  url "https://github.com/bavc/deckcontrol/archive/v0.3.tar.gz"
-  sha256 "3fe68d35a29051f81d997d4e28a4775f7b3c7899bdc87d60877c554fd1d4c816"
+  url "https://github.com/bavc/deckcontrol/archive/v0.4.tar.gz"
+  sha256 "511b71f139f045e78ca084ed3997a73c6a1c34ae0c02126e657c4120097974fa"
   head "https://github.com/bavc/deckcontrol.git"
 
   depends_on "decklinksdk" => :build
