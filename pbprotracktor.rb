83ab2e166459b88207679bb253d93423afffdd92
Reto Kromer <retokromer@users.noreply.github.com>
2020-07-17 19:55:06 +0200

    add license

--- pbprotracktor.rb
+++ pbprotracktor.rb
@@ -4,6 +4,7 @@ class Pbprotracktor < Formula
   url "https://github.com/amiaopensource/pbprotracktor/archive/v2020-04-30.tar.gz"
   version "2020-04-30"
   sha256 "9ffcd96fa5404f45f15602f1643fd6ecbbae8da65a519447f8641dc4ca1d6bf1"
+  license "MIT"
   head "https://github.com/amiaopensource/pbprotracktor.git"
 
   depends_on "jq"
