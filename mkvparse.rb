4791a6790929a8fbf08e55319699c4ceb7ad9b80
Reto Kromer <retokromer@users.noreply.github.com>
2020-10-17 11:07:45 +0200

    add license

--- mkvparse.rb
+++ mkvparse.rb
@@ -3,6 +3,7 @@ class Mkvparse < Formula
   homepage "https://github.com/vi/mkvparse"
   url "https://github.com/vi/mkvparse/archive/v1.2.1.tar.gz"
   sha256 "4050de75adf58e16796de9f684d0b30409b0e231c21f4930985a4c57a7bb9f4c"
+  license "MIT"
   head "https://github.com/vi/mkvparse.git"
 
   def install
