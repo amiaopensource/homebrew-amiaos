cc54b4538ccc072c5f40d98d7cf3812332924aff
רטו <retokromer@users.noreply.github.com>
2020-12-02 20:42:10 +0100

    ffmprovisr 2020-12-02

--- ffmprovisr.rb
+++ ffmprovisr.rb
@@ -1,9 +1,9 @@
 class Ffmprovisr < Formula
   desc "Repository of useful FFmpeg command-lines for archivists"
   homepage "https://github.com/amiaopensource/ffmprovisr"
-  url "https://github.com/amiaopensource/ffmprovisr/archive/v2020-11-12.tar.gz"
-  version "2020-11-12"
-  sha256 "73b04eaf174a2cac834afd470f7a69ee42d4a1a8d4353d57ec2b96971455fbf2"
+  url "https://github.com/amiaopensource/ffmprovisr/archive/v2020-12-02.tar.gz"
+  version "2020-12-02"
+  sha256 "b9bf16c4d6d62a7af47fc26c9f8d9e02ebf0ecc665396d8757845b212764f325"
 
   def install
     bin.install "scripts/ffmprovisr"
