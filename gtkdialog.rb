3694f35dd680b208cf90db294a5c0c1e821037be
Dave Rice <dave@dericed.com>
2021-04-26 15:17:23 -0400

    gtkdialog, upload to 0.8.4d, use same src for mac & linux (#283)

--- gtkdialog.rb
+++ gtkdialog.rb
@@ -1,16 +1,13 @@
 class Gtkdialog < Formula
   desc "Small utility for fast and easy GUI building"
   homepage "https://code.google.com/archive/p/gtkdialog/"
-  if OS.mac?
-    url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/gtkdialog/gtkdialog-0.8.3.tar.gz"
-    sha256 "ff89d2d7f1e6488e5df5f895716ac1d4198c2467a2a5dc1f51ab408a2faec38e"
-  else
-    url "https://github.com/puppylinux-woof-CE/gtkdialog/archive/0.8.4c.tar.gz"
-    sha256 "fba198ca6aeb347de4e403751245a4c29faa1a57102a2b3413bda1690266ba06"
-  end
-  revision 2
+  url "https://github.com/puppylinux-woof-CE/gtkdialog/archive/0.8.4d.tar.gz"
+  sha256 "1d3619ef1aca2baa783b936e8c6bd67135621f47428049c8231db9ee366f73db"
+  revision 3
 
   depends_on "atk" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "cairo" => :build
   depends_on "fontconfig" => :build
   depends_on "freetype" => :build
@@ -37,40 +34,18 @@ class Gtkdialog < Formula
   depends_on "pkg-config" => :build
   depends_on "xorgproto" => :build
 
-  on_linux do
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build  
-  end
-
   # Update patch to compile gtkdialog on macOS
   # See: http://www.murga-linux.com/puppy/viewtopic.php?t=108945
   patch :DATA if OS.mac?
 
   def install
-  	if OS.mac?
-      system "./configure"
-      system "make"
-    else
-      system "./autogen.sh"
-      system "make"
-    end
+    system "./autogen.sh"
+    system "make"
     bin.install "src/gtkdialog"
   end
 end
 
 __END__
-diff -urN a/src/automaton.c b/src/automaton.c
---- a/src/automaton.c	2017-11-16 07:27:06.000000000 +0200
-+++ b/src/automaton.c	2017-11-16 07:21:33.000000000 +0200
-@@ -1295,7 +1295,7 @@
-    Miert nem kapja meg az adatokat parameterben es foglalkozik
-    a veremmel a hivo?
-  */
--#ifdef __arm__
-+#if defined(__arm__) || defined(__APPLE__)
- /* 120701 BK Puppy Linux forum member jamesbond fixed this for arm cpus...*/
- stackelement _sum(stackelement b, stackelement a)
- #else
 diff -urN a/src/variables.c b/src/variables.c
 --- a/src/variables.c	2017-11-16 07:27:06.000000000 +0200
 +++ b/src/variables.c	2017-11-16 07:22:42.000000000 +0200
