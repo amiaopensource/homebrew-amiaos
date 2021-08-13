42143d0f40c0ed558d5c63b742f041da5f66db78
רטו • Reto <retokromer@users.noreply.github.com>
2021-08-13 19:40:24 +0200

    ffmprovisr 2021-08-13 (#297)

--- ffmprovisr.rb
+++ ffmprovisr.rb
@@ -1,9 +1,9 @@
 class Ffmprovisr < Formula
   desc "Repository of useful FFmpeg command-lines for archivists"
   homepage "https://github.com/amiaopensource/ffmprovisr"
-  url "https://github.com/amiaopensource/ffmprovisr/archive/v2020-12-02.tar.gz"
-  version "2020-12-02"
-  sha256 "b9bf16c4d6d62a7af47fc26c9f8d9e02ebf0ecc665396d8757845b212764f325"
+  url "https://github.com/amiaopensource/ffmprovisr/archive/v2021-08-13.tar.gz"
+  version "2021-08-13"
+  sha256 "a15ad7701a2818dbf32ebd2e9d947585a2819676c25c4323b807dab1ec70d8f9"
 
   def install
     bin.install "scripts/ffmprovisr"
