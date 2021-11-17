47129f9891aa43806ba60cbe97b208612eaad835
רטו • Reto <retokromer@users.noreply.github.com>
2021-11-17 07:33:04 +0100

    cable-bible_2021-11-16

--- cable-bible.rb
+++ cable-bible.rb
@@ -1,9 +1,9 @@
 class CableBible < Formula
   desc "Guide to cables and connectors used in audiovisual archiving"
   homepage "https://github.com/amiaopensource/cable-bible"
-  url "https://github.com/amiaopensource/cable-bible/archive/v2021-07-26.tar.gz"
-  version "2021-07-26"
-  sha256 "7091e689fc3634e13258a2bbaf7dbe52d1c15d22ecd11fbe15e06e3221b1280e"
+  url "https://github.com/amiaopensource/cable-bible/archive/v2021-11-16.tar.gz"
+  version "2021-11-16"
+  sha256 "d7c220dcc916f997bee5d7a54d12a56d210c6fd46f8084014301326aea8c7301"
 
   def install
     bin.install "cable-bible"
