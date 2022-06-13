728ecf7d6ffd4021596af68205472a06883ba23f
רטו • Reto <retokromer@users.noreply.github.com>
2022-06-13 17:21:04 +0200

    sccyou version 0.2

--- sccyou.rb
+++ sccyou.rb
@@ -1,8 +1,8 @@
 class Sccyou < Formula
   desc "Create scc or srt outputs from video files which contain EIA-608 data"
   homepage "https://github.com/amiaopensource/sccyou"
-  url "https://github.com/amiaopensource/sccyou/archive/refs/tags/v0.1.tar.gz"
-  sha256 "84f17aa09a86cc0e24287cc3942d0ddbec96b921ff1a71aa5d01bb75d2115a08"
+  url "https://github.com/amiaopensource/sccyou/archive/refs/tags/v0.2.tar.gz"
+  sha256 "52cd9b712fdb5629222d4aaa608dcfa0c870a73275c7fd56c6e3503f25dc3dab"
   head "https://github.com/amiaopensource/sccyou.git"
 
   depends_on "ffmpeg"
