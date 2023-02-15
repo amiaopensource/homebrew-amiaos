4936a4b7fea28ffaa6e04664d5d854ebd7de5bc1
Dave Rice <dave@dericed.com>
2023-02-15 16:31:44 -0500

    version 20230215

--- astataudit.rb
+++ astataudit.rb
@@ -1,8 +1,8 @@
 class Astataudit < Formula
   desc "Generate audiovisual metrics for audio via FFmpeg and BWF MetaEdit"
   homepage "https://github.com/amiaopensource/astataudit"
-  url "https://github.com/amiaopensource/astataudit/archive/refs/tags/20230126.tar.gz"
-  sha256 "967231f72a290192669ba1d19423120c80aed9924533e85d62a5b695020f3916"
+  url "https://github.com/amiaopensource/astataudit/archive/refs/tags/20230215.tar.gz"
+  sha256 "c3d89e6bdceb87ac293c54d14a618504b417796e1ac540b6c55fe041675a8a0b"
   head "https://github.com/amiaopensource/astataudit.git"
 
   depends_on "bwfmetaedit"
