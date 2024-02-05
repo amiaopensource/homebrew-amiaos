64ec7feaefa4325bdffceaedac72fc73d05cf3c1
Dave Rice <dave@dericed.com>
2024-02-05 16:16:49 -0500

    sccyou v0.3

--- sccyou.rb
+++ sccyou.rb
@@ -1,8 +1,8 @@
 class Sccyou < Formula
   desc "Create scc or srt outputs from video files which contain EIA-608 data"
   homepage "https://github.com/amiaopensource/sccyou"
-  url "https://github.com/amiaopensource/sccyou/archive/refs/tags/v0.2.tar.gz"
-  sha256 "52cd9b712fdb5629222d4aaa608dcfa0c870a73275c7fd56c6e3503f25dc3dab"
+  url "https://github.com/amiaopensource/sccyou/archive/refs/tags/v0.3.tar.gz"
+  sha256 "dfdf6985a29a4183a81b31bda2cd2c68614b8c2646fa4a4f6f39887635bbe44f"
   head "https://github.com/amiaopensource/sccyou.git"
 
   depends_on "ffmpeg"
