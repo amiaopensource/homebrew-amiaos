b5c331a776e4f82d2718a0191814f8b7158edf27
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2025-04-17 14:56:41 +0200

    ffmprovisr 2025-04-16 (#396)

--- ffmprovisr.rb
+++ ffmprovisr.rb
@@ -1,7 +1,7 @@
 class Ffmprovisr < Formula
   desc "Repository of useful FFmpeg command-lines for archivists"
   homepage "https://github.com/amiaopensource/ffmprovisr"
-  url "https://github.com/amiaopensource/ffmprovisr/refs/tags/v2025-04-16.tar.gz"
+  url "https://github.com/amiaopensource/ffmprovisr/archive/v2025-04-16.tar.gz"
   sha256 "6b01b95e483e8dbd4253bbf2cf4a23b75bdd0b895ab497c1c66c6b9fd17ad8e2"
 
   def install
