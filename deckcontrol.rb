6f0b2c76d465b1124cef97e50c0d34135c2944a7
Maxime Gervais <gervais.maxime@gmail.com>
2025-04-21 18:39:53 +0200

    Fix deckcontrol v0.5 build with llvm 17 (#397)
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- deckcontrol.rb
+++ deckcontrol.rb
@@ -1,9 +1,13 @@
 class Deckcontrol < Formula
   desc "Control an attached videodeck via Blackmagic Design Decklink SDK"
   homepage "https://github.com/bavc/deckcontrol"
-  url "https://github.com/bavc/deckcontrol/archive/v0.5.tar.gz"
-  sha256 "73abcbb1801b11aaf954567fe8f86edd2efbe789037774653de6f06b219d48e7"
   head "https://github.com/bavc/deckcontrol.git"
+
+  stable do
+    url "https://github.com/bavc/deckcontrol/archive/refs/tags/v0.5.tar.gz"
+    sha256 "73abcbb1801b11aaf954567fe8f86edd2efbe789037774653de6f06b219d48e7"
+    patch :DATA
+  end
 
   depends_on "amiaopensource/amiaos/decklinksdk" => :build
 
@@ -12,3 +16,17 @@ class Deckcontrol < Formula
     bin.install "deckcontrol"
   end
 end
+__END__
+diff --git a/deckcontrol.cpp b/deckcontrol.cpp
+index 934680b..5eff492 100644
+--- a/deckcontrol.cpp
++++ b/deckcontrol.cpp
+@@ -269,7 +269,7 @@ int main(int argc, char *argv[])
+         uint8_t hours, minutes, seconds, frames;
+         deckControl->GetTimecode(&currentTimecode, &deckError);
+         currentTimecode->GetComponents(&hours, &minutes, &seconds, &frames);
+-        printf("TC=%02"PRIu8":%02"PRIu8":%02"PRIu8":%02"PRIu8"\n",
++        printf("TC=%02" PRIu8 ":%02" PRIu8 ":%02" PRIu8 ":%02" PRIu8 "\n",
+                hours, minutes, seconds, frames);
+         SAFE_RELEASE(currentTimecode);
+         break;
