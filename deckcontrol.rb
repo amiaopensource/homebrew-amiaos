80789603f3ec1541df06220fa3c3668f9fec1f5a
Dave Rice <dave@dericed.com>
2025-04-22 09:57:42 -0400

    deckcontrol v0.6

--- deckcontrol.rb
+++ deckcontrol.rb
@@ -1,13 +1,9 @@
 class Deckcontrol < Formula
   desc "Control an attached videodeck via Blackmagic Design Decklink SDK"
   homepage "https://github.com/bavc/deckcontrol"
+  url "https://github.com/bavc/deckcontrol/archive/refs/tags/v0.6.tar.gz"
+  sha256 "e4a184ff6ec64aef8cccd15712797e7507d2de1ce873396703d575047ae1463e"
   head "https://github.com/bavc/deckcontrol.git"
-
-  stable do
-    url "https://github.com/bavc/deckcontrol/archive/refs/tags/v0.5.tar.gz"
-    sha256 "73abcbb1801b11aaf954567fe8f86edd2efbe789037774653de6f06b219d48e7"
-    patch :DATA
-  end
 
   depends_on "amiaopensource/amiaos/decklinksdk" => :build
 
@@ -16,17 +12,3 @@ class Deckcontrol < Formula
     bin.install "deckcontrol"
   end
 end
-__END__
-diff --git a/deckcontrol.cpp b/deckcontrol.cpp
-index 934680b..5eff492 100644
---- a/deckcontrol.cpp
-+++ b/deckcontrol.cpp
-@@ -269,7 +269,7 @@ int main(int argc, char *argv[])
-         uint8_t hours, minutes, seconds, frames;
-         deckControl->GetTimecode(&currentTimecode, &deckError);
-         currentTimecode->GetComponents(&hours, &minutes, &seconds, &frames);
--        printf("TC=%02"PRIu8":%02"PRIu8":%02"PRIu8":%02"PRIu8"\n",
-+        printf("TC=%02" PRIu8 ":%02" PRIu8 ":%02" PRIu8 ":%02" PRIu8 "\n",
-                hours, minutes, seconds, frames);
-         SAFE_RELEASE(currentTimecode);
-         break;
