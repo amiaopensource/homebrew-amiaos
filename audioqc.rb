57ef5e9464f98228bd19962a7093976586baa709
Reto Kromer <retokromer@users.noreply.github.com>
2025-04-25 13:31:05 +0200

    audioqc: housekeeping
    - version redundant

--- audioqc.rb
+++ audioqc.rb
@@ -2,7 +2,6 @@ class Audioqc < Formula
   desc "Tool for aiding automated audio quality control"
   homepage "https://github.com/amiaopensource/audioqc"
   url "https://github.com/amiaopensource/audioqc/archive/refs/tags/2023-11-22.tar.gz"
-  version "2023-11-22"
   sha256 "5a98e8836c427b5e238c9e591c0f4139ead37d0dc32d22d07cd6a20d7dcb8fe7"
   head "https://github.com/amiaopensource/audioqc.git"
 
