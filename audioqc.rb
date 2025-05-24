7b8d11265374d04c6efc3019d6803b369b6f9d17
רטו\רעטאָ\רֵיטוֹ • Reto <retokromer@users.noreply.github.com>
2025-05-24 06:36:00 +0200

    audioqc 2025-05-23

--- audioqc.rb
+++ audioqc.rb
@@ -1,8 +1,8 @@
 class Audioqc < Formula
   desc "Tool for aiding automated audio quality control"
   homepage "https://github.com/amiaopensource/audioqc"
-  url "https://github.com/amiaopensource/audioqc/archive/refs/tags/2023-11-22.tar.gz"
-  sha256 "5a98e8836c427b5e238c9e591c0f4139ead37d0dc32d22d07cd6a20d7dcb8fe7"
+  url "https://github.com/amiaopensource/audioqc/archive/refs/tags/2025-05-23.tar.gz"
+  sha256 "42fff6ebbd6723d930f4458a4ad799e899d69eeaec25f19981e3467e9176243e"
   head "https://github.com/amiaopensource/audioqc.git"
 
   on_macos do
