e05c682275359c0fbab9466641b84b2984b3ec2f
Andrew Weaver <theandrewjw@gmail.com>
2025-05-27 23:52:31 -0700

    audioqc: 2025-05-27 release (#406)

--- audioqc.rb
+++ audioqc.rb
@@ -1,28 +1,28 @@
 class Audioqc < Formula
   desc "Tool for aiding automated audio quality control"
   homepage "https://github.com/amiaopensource/audioqc"
-  url "https://github.com/amiaopensource/audioqc/archive/refs/tags/2023-11-22.tar.gz"
-  sha256 "5a98e8836c427b5e238c9e591c0f4139ead37d0dc32d22d07cd6a20d7dcb8fe7"
+  url "https://github.com/amiaopensource/audioqc/archive/refs/tags/2025-05-27.tar.gz"
+  sha256 "6fab74b658e316d0a8b178647383ca41eabb273967de2b227232d1d303466ad9"
   head "https://github.com/amiaopensource/audioqc.git"
 
   on_macos do
-    depends_on "bwfmetaedit"
     depends_on "mediaconch"
     depends_on "mediainfo"
+    depends_on "ffmpeg"
   end
 
   on_linux do
     def caveats
       <<~EOS
         ** IMPORTANT FOR LINUX INSTALL **
-        Additional dependencies BWFMetaedit, MediaInfo and MediaConch must be installed prior to use.
+        Additional dependencies FFmpeg, FFprobe, MediaInfo and MediaConch must be installed prior to use.
       EOS
     end
   end
 
   def install
     bin.install "audioqc"
-    bin.install "audioqc.config"
-    bin.install "makespectrums"
+    bin.install "audioqc_methods.rb"
+    bin.install "settings.csv"
   end
 end
