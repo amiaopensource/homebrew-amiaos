b7569ff96dffc997d31cffe529b021c7d2c4a348
Dave Rice <dave@dericed.com>
2026-03-16 15:27:29 -0400

    pbprotracktor v2026-03-16

--- pbprotracktor.rb
+++ pbprotracktor.rb
@@ -1,14 +1,14 @@
 class Pbprotracktor < Formula
   desc "Postgres calls to databases to produce a PBCore XML or CSV output"
   homepage "https://github.com/amiaopensource/pbprotracktor"
-  url "https://github.com/amiaopensource/pbprotracktor/archive/refs/tags/v2020-04-30.tar.gz"
-  sha256 "9ffcd96fa5404f45f15602f1643fd6ecbbae8da65a519447f8641dc4ca1d6bf1"
+  url "https://github.com/amiaopensource/pbprotracktor/archive/refs/tags/v2026-03-16.tar.gz"
+  sha256 "5f060b6634b70470bda4691d4eb7405cb359cae9c562006a1f9d39d81e245675"
   license "MIT"
   head "https://github.com/amiaopensource/pbprotracktor.git"
 
   depends_on "jq"
   depends_on "mysql"
-  depends_on "postgresql@14"
+  depends_on "postgresql@18"
   depends_on "xmlstarlet"
 
   def install
