1cd29aaa87f4afeb16285d5d66923b614ebe3319
Andrew Weaver <theandrewjw@gmail.com>
2026-05-20 11:21:29 -0700

    audioqc 2026-05-20 (#456)

--- audioqc.rb
+++ audioqc.rb
@@ -1,10 +1,9 @@
 class Audioqc < Formula
   desc "Tool for aiding automated audio quality control"
   homepage "https://github.com/amiaopensource/audioqc"
-  url "https://github.com/amiaopensource/audioqc/archive/refs/tags/2025-05-27.tar.gz"
-  sha256 "6fab74b658e316d0a8b178647383ca41eabb273967de2b227232d1d303466ad9"
+  url "https://github.com/amiaopensource/audioqc/archive/refs/tags/2026-05-20.tar.gz"
+  sha256 "371c8531b41b2e9ef774e540fddc598e0c7dc45c8f915fd4dfe87395d106f5ce"
   head "https://github.com/amiaopensource/audioqc.git"
-  revision 1
 
   depends_on "ffmpeg"
   depends_on "mediaconch"
